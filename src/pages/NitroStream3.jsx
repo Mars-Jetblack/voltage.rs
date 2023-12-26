aff052722acbab696dc287dabf8cb774679249d4
Mars Jetblack <brisa4101@gmail.com>
2023-12-26 18:37:06 -0300

    Updated again NS3 link

--- src/pages/NitroStream3.jsx
+++ src/pages/NitroStream3.jsx
@@ -7,7 +7,7 @@ const NitroStream3 = () =>{
             gameTitle={"NitroStream 3: Ignition"}
             gameDescription={"Fight the Triad to recover your safehouse, while you get the help of an anonymous sponsor"}
             embedYouTubeId={"J_Ta9zq2diA?si=gkjiqs0CxnKSnh-j"}
-            downloadPath={"https://drive.google.com/uc?id=1PsaR9HlBPD-6vJpX2bxpyTFBn3jnfmLF"}
+            downloadPath={"https://drive.google.com/uc?id=15C4ueGfvU4QcDY4RBJG2QnPJ2eoBhN2p"}
             gameVersion={"1.0.0"}
             versionDate={"2023-12"}
         />  
